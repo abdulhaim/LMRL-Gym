2a986c975b0e7f920846eaf3ad6eae775ff01141
Isadora White <isadora.c.white@gmail.com>
2023-12-30 17:16:02 -0500

    successfully added encoder and decoder weights

--- llm_rl_scripts/convert_flax_to_pytorch.py
+++ llm_rl_scripts/convert_flax_to_pytorch.py
@@ -5,22 +5,40 @@ from transformers import T5ForConditionalGeneration, FlaxT5ForConditionalGenerat
 from JaxSeq.bucket_manager import open_with_bucket as open
 from JaxSeq.models.T5.load import load_params as t5_load_params, ModelLoadMode as T5ModelLoadMode
 from JaxSeq.utils import jsonl_stream, convert_path, load_mesh, get_dtype
+import jax
+from IPython import embed
+import os
 
-pt_model = T5ForConditionalGeneration.from_pretrained("google/flan-t5-xl", 
-                                                    #   vocab_size=32768, 
-                                                    #   ignore_mismatched_sizes=True
-                                                      )
-flax_checkpoint_path = "gcs://rl-llm-bench-dataset-internal/guess-my-city/simulator/model/"
-# import correct flax class
-# flax_cls = getattr(transformers, "Flax" + model.__class__.__name__)
-params, model = t5_load_params(
-        model_load_mode=T5ModelLoadMode.PARAMS,
-        model_load_path=flax_checkpoint_path,
-        tokenizer="google/flan-t5-xl",
-        mesh=load_mesh((1, 1, -1), ('dp', 'fsdp', 'mp')),
-        model_dtype=get_dtype(use_fp16=True),
-        params_dtype=get_dtype(use_fp16=True)
-    )
+def load_t5_pytorch_model(flax_checkpoint_path):
+    pt_model = T5ForConditionalGeneration.from_pretrained("google/flan-t5-xl", architectures=["T5ForConditionalGeneration"])
 
-load_flax_weights_in_pytorch_model(pt_model, params)
+    params, model = t5_load_params(
+            model_load_mode=T5ModelLoadMode.PARAMS,
+            model_load_path=flax_checkpoint_path,
+            tokenizer="google/flan-t5-xl",
+            mesh=load_mesh((1, 1, -1), ('dp', 'fsdp', 'mp')),
+            model_dtype=get_dtype(use_fp16=True),
+            params_dtype=get_dtype(use_fp16=True)
+        )
+    start_indices = (0, 0)
+    params["lm_head"]["kernel"] = jax.lax.dynamic_slice(params["lm_head"]["kernel"], start_indices, pt_model.state_dict()["lm_head.weight"].T.shape)
+    params["shared"]["embedding"] = jax.lax.dynamic_slice(params["shared"]["embedding"], start_indices, pt_model.state_dict()["shared.weight"].shape)
+
+    print("encoder size: ", pt_model.state_dict()["encoder.embed_tokens.weight"].shape)
+    print("decoder size: ", pt_model.state_dict()["decoder.embed_tokens.weight"].shape)
+
+    print("shared embedding size: ", params["shared"]["embedding"].shape)
+    print("lm_head kernel size: ", params["lm_head"]["kernel"].shape)
+
+    new_model = load_flax_weights_in_pytorch_model(pt_model, params)
+
+    new_model["encoder.embed_tokens.weight"] = new_model["shared.weight"].copy()
+    new_model["decoder.embed_tokens.weight"] = new_model["shared.weight"].copy()
+    return new_model
+
+model_load_path = "gcs://rl-llm-bench-dataset-internal/guess-my-city/simulator/model/"
+save_path = os.getcwd() + "/outputs/guess_city_pytorch_model.pt"
+new_model = load_t5_pytorch_model(model_load_path)
+embed()
+new_model.save_pretrained(save_path)
 # loaded_model = load_flax_checkpoint_in_pytorch_model(model, "gcs://rl-llm-bench-dataset-internal/guess-my-city/simulator/model/")
