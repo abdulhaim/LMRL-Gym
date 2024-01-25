aac32dc2945426c7233a818fd7778e7fb5d296bb
Isadora White <isadora.c.white@gmail.com>
2024-01-25 14:48:08 -0800

    fixed twenty questions ilql

--- experiments/twenty_questions/ilql.sh
+++ experiments/twenty_questions/ilql.sh
@@ -1,7 +1,7 @@
-export GOOGLE_APPLICATION_CREDENTIALS="/nfs/nfs1/users/isadorawhite/rail-tpus.json"
+export GOOGLE_APPLICATION_CREDENTIALS="/nfs/nfs2/users/isadorawhite/rail-tpus.json"
 export GCLOUD_PROJECT="rail-tpus"
 export TOKENIZERS_PARALLELISM=false
 sudo rm -r /tmp/*tpu*
-conda init bash
 conda activate ./gym_venv
-python -m llm_rl_scripts.twenty_questions.ilql.train_ilql HF gpt2-medium gcs://rl-llm-bench-dataset-internal/twenty-questions/train.json gcs://rl-llm-bench-dataset-internal/twenty-questions/eval.json HF gcs://rl-llm-bench-dataset-internal/twenty-questions/oracle --outputs_path=gcs://rail-tpus-isadora/test-twenty-questions/ilql/ --data-mesh-shape 1 --model-mesh-shape 8 --epochs 1 --use-wandb --wandb-project twenty_questions
+sudo chmod -R 777 .
+python -m llm_rl_scripts.twenty_questions.ilql.train_ilql HF gpt2-medium gcs://rl-llm-bench-dataset-internal/twenty-questions/train.json gcs://rl-llm-bench-dataset-internal/twenty-questions/eval.json gcs://rl-llm-bench-dataset-internal/twenty-questions/oracle --outputs_path=gcs://rail-tpus-isadora/twenty-questions/ilql/ --eval-every-steps 512 --data-mesh-shape 4 --model-mesh-shape 1 --eval-at-beginning --epochs 1 --use-wandb --wandb-project twenty_questions
